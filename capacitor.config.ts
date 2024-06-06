b1ead7debca36bc634eb61f7870f0f5809a5e27f
gessnerbezerra <79493437+GessnerBezerra@users.noreply.github.com>
2024-06-06 00:06:49 +0000

    capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.clinicaEstetica.app',
+  appName: 'adrimelo-tricologia',
+  webDir: 'dist'
+};
+
+export default config;
